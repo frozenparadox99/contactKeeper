44b3af6df0dfc12220af2b3a72e891856c3f395b
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-03-26 10:59:37 +0530

    deployment prep

---
+++ README.md
@@ -0,0 +1,30 @@
+# Contact Keeper
+
+> Full stack MERN contact manager with React hooks, context & JWT authentication.
+
+This is the full app.
+
+## Website Image
+
+![What a user sees once logged in](https://lh3.googleusercontent.com/ODqALY2hFGYZ4lagBIPjvJ9cvQ75D3a3G4EgxdI7haqImv3vRD9tZ76jwGoN7nAPqKQX62Wo5CRIwkLexf_-kb2EruWiOjP7MeyKy9sdyziDNqhtiE-MLCyqqqItGs52wgcppXoBYMx3Sf9qspVIvIgREt829TXHDWePnc1Y-OzKE0Mfl3bPBeVA2p1xjiGq0KoIDzwd_hJ-o6LrjrRVB1VQ-hDFH3CqHw4mcBmmInvuhjPK2Va0dDEPfits_BvenJPaKbb9bT-K-A2mIawcTbHYDAduGCqWCD1qGI0-B5xglCRidT6efnSHCdT-f9s1BHC29arcvglGSkPl7EeFNyDHDVp6u2g7PuejDXWOZ5ST2sRXYHARDgBDfL2HIqMq435K-7oA264Z9exiOTdyEHoHNl_btjjGU8QaJ3syR4zjcU7cT2jmWdeA3vh2dqGTgP_IQNGlfCRvfo8awuUyx3yD9Kn62hqlqIJ3kiKguCb8-wkkOGdzCmJZ8oi46evxmRU_9OxOIxhS4U9UGCQm2H57M1eGtn2RccG89kO54JwjllzuXjpMUc5CQtNyDYrsW0mIlShdUn7Bk932E11srE8n6sqnHRPUw79sL5Tm6O8mMKWS-1jtwWKwbBQti4lZ5mhFbHkqt0FUkWQRHcUvarB-5P4SaSrEe0Rw2mcM3LBcquIhXfcWp4kvUydjPNapmI1XhUPDo1bTEgye3sEuMvD1Kpb5ars13S0NvlimXKYtaxo4IsbpPw=w1919-h739-no)
+
+## Usage
+
+Install dependencies
+
+```bash
+npm install
+npm client-install
+```
+
+### Mongo connection setup
+
+Edit your /config/default.json file to include the correct MongoDB URI
+
+### Run Server
+
+```bash
+npm run dev     # Express & React :3000 & :5000
+npm run server  # Express API Only :5000
+npm run client  # React Client Only :3000
+```
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,57 +1,65 @@
 import React, { useReducer } from "react";
 import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 import ContactContext from "./contactContext";
 import ContactReducer from "./contactReducer";
 import {
+  GET_CONTACTS,
   ADD_CONTACT,
   DELETE_CONTACT,
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
-  CLEAR_FILTER
+  CLEAR_CONTACTS,
+  CLEAR_FILTER,
+  CONTACT_ERROR
 } from "../types";
 
 const ContactState = props => {
   const initialState = {
-    contacts: [
-      {
-        id: 1,
-        name: "Naruto",
-        email: "theseventh@gmail.com",
-        phone: "111-111-1111",
-        type: "professional"
-      },
-      {
-        id: 2,
-        name: "Sasuke",
-        email: "chidori@gmail.com",
-        phone: "222-222-2222",
-        type: "personal"
-      },
-      {
-        id: 3,
-        name: "Sakura",
-        email: "ilovesasuke@gmail.com",
-        phone: "333-333-3333",
-        type: "personal"
-      }
-    ],
+    contacts: null,
     current: null,
-    filtered: null
+    filtered: null,
+    error: null
   };
 
   const [state, dispatch] = useReducer(ContactReducer, initialState);
 
+  // Get contacts
+  const getContacts = async () => {
+    try {
+      const res = await axios.get("/api/contacts");
+      console.log(res.data);
+      dispatch({ type: GET_CONTACTS, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
+  };
+
   //Add contact
-  const addContact = contact => {
-    contact.id = uuidv4();
-    dispatch({ type: ADD_CONTACT, payload: contact });
+  const addContact = async contact => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    try {
+      const res = await axios.post("/api/contacts", contact, config);
+      dispatch({ type: ADD_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
   };
 
   //delete contact
-  const deleteContact = id => {
-    dispatch({ type: DELETE_CONTACT, payload: id });
+  const deleteContact = async id => {
+    try {
+      await axios.delete(`/api/contacts/${id}`);
+      dispatch({ type: DELETE_CONTACT, payload: id });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
   };
 
   //set current contact
@@ -65,8 +73,22 @@ const ContactState = props => {
   };
 
   //update contact
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
   };
 
   //filter contacts
@@ -79,19 +101,27 @@ const ContactState = props => {
     dispatch({ type: CLEAR_FILTER });
   };
 
+  //Clear Contacts
+  const clearContacts = () => {
+    dispatch({ type: CLEAR_CONTACTS });
+  };
+
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
         filtered: state.filtered,
+        error: state.error,
+        getContacts,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
         updateContact,
         filterContacts,
-        clearFilter
+        clearFilter,
+        clearContacts
       }}
     >
       {props.children}
